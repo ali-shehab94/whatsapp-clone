162da00b4538f7d9249c36fc589fd1d1571255da
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-12-09 00:04:47 +0200

    Create page container

---
+++ WC-frontend/screens/AuthScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import PageContainer from '../components/PageContainer';
+
+const AuthScreen = (props) => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+        <PageContainer>
+            <Text>Hello</Text>
+        </PageContainer>
+    </SafeAreaView>
+    
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default AuthScreen
